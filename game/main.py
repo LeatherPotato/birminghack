e6668a8e311913fbbb9b604bc903ba633ca9087b
cacti <owsome.jedi@gmail.com>
2025-03-22 18:52:49 +0000

    stuuf and things

--- game/main.py
+++ game/main.py
@@ -8,10 +8,11 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode([600, 500])
 
 class TextBox:
-    def __init__(self,x,y,width,height,colour,text):
+    def __init__(self,x,y,width,height,colour,text,font):
         self.rect = pygame.Rect(x,y,width,height)
         self.colour = colour
         self.text = text
+        self.font = font
 
     def setColour(self,colour):
         self.colour = colour
@@ -19,14 +20,55 @@ class TextBox:
     def clicked(self):
         pass
 
+    def draw(self,surface):
+        self.rect.center = (surface.get_width()//2,surface.get_height()//2)
+        pygame.draw.rect(surface,self.colour,self.rect) 
+        textSurface = self.font.render(self.text, True, (0,0,0)) 
+        surface.blit(textSurface, (self.rect.x+5,self.rect.y+5)) 
+        self.rect.w = max(100, textSurface.get_width()+10)
+        
+
 class Button:
-    def __init__(self,x,y,width,height,colour,label):
+    def __init__(self,x,y,width,height,colour,label,font):
         self.rect = pygame.Rect(x,y,width,height)
         self.colour = colour
         self.label = label
+        self.font = font
+        self.border = 1
+        self.lastClicked = -5000
 
     def setColour(self,colour):
         self.colour = colour
+
+    def draw(self,surface,offset):
+        self.rect.center = (surface.get_width()//2 + offset[1],surface.get_height()//2 + offset[0])
+        pygame.draw.rect(surface,self.colour,self.rect)
+        textSurface = self.font.render(self.label, True, (0,0,0)) 
+        surface.blit(textSurface, (self.rect.x+5,self.rect.y+5))
+        self.rect.w = max(100, textSurface.get_width()+10)
+        pygame.draw.rect(surface,(255,255,255),self.rect,self.border)
+        
+
+    def clicked(self,event):
+        self.hover()
+        returner = False
+        if event.type == pygame.MOUSEBUTTONDOWN and pygame.time.get_ticks() - self.lastClicked > 500:
+            if self.rect.collidepoint(event.pos):
+                self.border = 1
+                self.lastClicked = pygame.time.get_ticks()
+                returner =  True
+        return returner
+
+    def hover(self):
+        if self.rect.collidepoint(pygame.mouse.get_pos()):
+            self.border = 3
+            self.colour = pygame.Color('lightskyblue3')
+        else:
+            self.border = 1
+            self.colour = pygame.Color('chartreuse4')
+        
+
+
 
 class Image:
     def __init__(self,x,y,width,height,image):
@@ -50,8 +92,13 @@ class CharSelect:
         if select == self.name:
             pygame.draw.rect(surface,(255,255,255),box,2)
 
-    def clicked(self,event):
-        pass
+    def clicked(self,event,select):
+        box = self.scaledImage.get_rect()
+        box.center = self.pos
+        if box.collidepoint(event.pos):
+            return self.name
+        else:
+            return select
 
 
 
@@ -60,9 +107,9 @@ base_font = pygame.font.Font(None, 32)
 colour_active = pygame.Color('lightskyblue3')
 colour_passive = pygame.Color('chartreuse4')
 
-gameIDInput = TextBox(200,200,140,32,colour_passive,'')
-createGame = Button(200,100,140,32,colour_passive,'Create new game!')
-joinGame = Button(200,300,140,32,colour_passive,'Join game with code!')
+gameIDInput = TextBox(200,200,140,32,colour_passive,'',base_font)
+createGame = Button(200,100,140,32,colour_passive,'Create new game!',base_font)
+joinGame = Button(200,300,140,32,colour_passive,'Join game with code!',base_font)
 char1 = CharSelect(200,440,50,50,'barry.png','Flash')
 char2 = CharSelect(300,440,50,50,'barry.png','Buzz')
 char3 = CharSelect(400,440,50,50,'barry.png','Sensor')
@@ -81,26 +128,28 @@ while gameRun:
                 gameRun = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                    if gameIDInput.rect.collidepoint(event.pos):
-                            active = True
-                    else: 
-                            active = False
-                    selected = char1.clicked(event,selected)
-                    selected = char2.clicked(event,selected)
-                    selected = char3.clicked(event,selected)
+                if gameIDInput.rect.collidepoint(event.pos):
+                        active = True
+                else: 
+                        active = False
+
+                selected = char1.clicked(event,selected)
+                selected = char2.clicked(event,selected)
+                selected = char3.clicked(event,selected)
+                createGame.clicked(event)
+                joinGame.clicked(event)
 
             if (event.type == pygame.KEYDOWN) and active:
+                
+                if event.key == pygame.K_BACKSPACE: 
 
-                    # Check for backspace 
-                    if event.key == pygame.K_BACKSPACE: 
+                        # get text input from 0 to -1 i.e. end. 
+                        gameIDInput.text = gameIDInput.text[:-1] 
 
-                            # get text input from 0 to -1 i.e. end. 
-                            gameIDInput.text = gameIDInput.text[:-1] 
-
-                    # Unicode standard is used for string 
-                    # formation 
-                    else: 
-                            gameIDInput.text += event.unicode
+                # Unicode standard is used for string 
+                # formation 
+                else: 
+                        gameIDInput.text += event.unicode
         
         # it will set background color of screen 
         screen.fill((0,0,0))
@@ -110,16 +159,10 @@ while gameRun:
         else: 
                 gameIDInput.setColour(colour_passive)
 
-        pygame.draw.rect(screen, gameIDInput.colour, gameIDInput.rect) 
+        gameIDInput.draw(screen)
 
-        text_surface = base_font.render(gameIDInput.text, True, (0,0,0)) 
-        
-        # render at position stated in arguments 
-        screen.blit(text_surface, (gameIDInput.rect.x+5, gameIDInput.rect.y+5)) 
-        
-        # set width of textfield so that text cannot get 
-        # outside of user's text input 
-        gameIDInput.rect.w = max(100, text_surface.get_width()+10)
+        createGame.draw(screen,(-100,0))
+        joinGame.draw(screen,(100,0))
 
         char1.draw(screen,selected)
         char2.draw(screen,selected)
