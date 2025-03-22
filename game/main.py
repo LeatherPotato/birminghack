a8f59a8ea49f187612f51264d0d6cc99d81d818b
cacti <owsome.jedi@gmail.com>
2025-03-22 16:10:48 +0000

    studf

--- game/main.py
+++ game/main.py
@@ -7,24 +7,51 @@ clock = pygame.time.Clock()
 # it will display on screen 
 screen = pygame.display.set_mode([600, 500])
 
-class UI:
-    def __init__(self,x,y,width,height,colour):
+class TextBox:
+    def __init__(self,x,y,width,height,colour,text):
         self.rect = pygame.Rect(x,y,width,height)
         self.colour = colour
+        self.text = text
 
-    def setColour(colour):
+    def setColour(self,colour):
         self.colour = colour
 
-base_font = pygame.font.Font(None, 32) 
-gameID = ''
-rap = ''
+class Button:
+    def __init__(self,x,y,width,height,colour,label):
+        self.rect = pygame.Rect(x,y,width,height)
+        self.colour = colour
+        self.label = label
+
+    def setColour(self,colour):
+        self.colour = colour
+
+class Image:
+    def __init__(self,x,y,width,height,image):
+        self.rect = pygame.Rect(x,y,width,height)
+        self.image = pygame.image.load(image)
+
+    def setImage(self,image):
+        self.image = image
+
+class CharSelect:
+    def __init__(self,x,y,width,height,image):
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect(topleft=(x,y))
+
+    def draw(self,surface):
+        surface.blit(self.image,self.rect)
+
+
+
+base_font = pygame.font.Font(None, 32)
 
 colour_active = pygame.Color('lightskyblue3')
-colour_passive = pygame.Color('chartreuse4') 
+colour_passive = pygame.Color('chartreuse4')
 
-gameIDInput = UI(200,200,140,32,colour_passive)
-createGame = UI(200,100,140,32,colour_passive)
-joinGame = UI(200,300,140,32,colour_passive)
+gameIDInput = TextBox(200,200,140,32,colour_passive,'')
+createGame = Button(200,100,140,32,colour_passive,'Create new game!')
+joinGame = Button(200,300,140,32,colour_passive,'Join game with code!')
+char1 = CharSelect(150,400,50,50,'barry.png')
 
 active = False
 gameRun = True
@@ -50,12 +77,12 @@ while gameRun:
                     if event.key == pygame.K_BACKSPACE: 
 
                             # get text input from 0 to -1 i.e. end. 
-                            gameID = gameID[:-1] 
+                            gameIDInput.text = gameIDInput.text[:-1] 
 
                     # Unicode standard is used for string 
                     # formation 
                     else: 
-                            gameID += event.unicode
+                            gameIDInput.text += event.unicode
         
         # it will set background color of screen 
         screen.fill((0,0,0))
@@ -65,9 +92,9 @@ while gameRun:
         else: 
                 gameIDInput.setColour(colour_passive)
 
-        pygame.draw.rect(screen, colour, gameIDInput.rect) 
+        pygame.draw.rect(screen, gameIDInput.colour, gameIDInput.rect) 
 
-        text_surface = base_font.render(gameID, True, (0,0,0)) 
+        text_surface = base_font.render(gameIDInput.text, True, (0,0,0)) 
         
         # render at position stated in arguments 
         screen.blit(text_surface, (gameIDInput.rect.x+5, gameIDInput.rect.y+5)) 
@@ -75,6 +102,8 @@ while gameRun:
         # set width of textfield so that text cannot get 
         # outside of user's text input 
         gameIDInput.rect.w = max(100, text_surface.get_width()+10)
+
+        char1.draw(screen)
         
         # display.flip() will update only a portion of the 
         # screen to updated, not full area
