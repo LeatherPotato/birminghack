ad9ebfaa3ef20df8cab141b80af88ee9ff303ac7
LeatherPotato <thesussywussy06@gmail.com>
2025-03-23 11:28:03 +0000

    pushin

--- game/network.py
+++ game/network.py
@@ -4,24 +4,30 @@ import json
 
 class Network:
     def __init__(self, websocket):
-        self.SERVER = "ws://172.22.236.99:8765"
+        # self.SERVER = "ws://172.22.236.99:8765"
         self.websocket = websocket
 
-    def _request_sender(self, req_type, data):
+    def request_sender(self, req_type, data):
+        print("sending request...")
         try:
+            print("sending request 2...")
             self.websocket.send(json.dumps({"request_type": req_type, "data": data}))
+            print("sending request 3...")
             response = self.websocket.recv()
+            print("sending request 4...")
             return json.load(response)
         except:
-            pass
+            print("oh no eror :()")
 
     def create_lobby(self, playerInfo):
-        response = self._request_sender(self, "CREATE_LOBBY", playerInfo)
+        print("started creating lobby")
+        response = self.request_sender(self, "CREATE_LOBBY", playerInfo)
+        print("got response from request builder")
         return response["code"]
     
 
     def join_lobby(self, playerInfo, code):
-        response = self._request_sender(self, "JOIN_LOBBY", {"code": code, "player_info": playerInfo})
+        response = self.request_sender(self, "JOIN_LOBBY", {"code": code, "player_info": playerInfo})
         
         if response["response_type"] == "ERROR":
             return -1
@@ -30,6 +36,6 @@ class Network:
         
     
     def submit_rap(self, code, player, rap):
-        response = self._request_sender(self, "SUBMIT_RAP", {"code": code, "player": player, "rap": rap})
+        response = self.request_sender(self, "SUBMIT_RAP", {"code": code, "player": player, "rap": rap})
         return response
      
